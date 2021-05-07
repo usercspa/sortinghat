2bb59b3d6f7173db94d4c64222ea5f5d724bc798
usercspa <>
2021-05-07 23:21:20 +0000

    random allocation

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 # install packages
-import pygame 
+import random
 import time 
 from time import sleep 
-import random 
-import serial #pyserial 
-
-#initialize
-pygame.init()
-
-#screen
-WIDTH = 800
-HEIGHT = 600
-
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Sorting hat")
-clock = pygame.time.Clock()
+import tkinter #GUI
 
 #colors
 black = (0,0,0)
@@ -22,3 +10,29 @@ white = (255,255,255)
 red = (255,0,0)
 grey = (123,123,123)
 
+houses = ["Slytherin", "Gryffindor", "Hufflepuff", "Ravenclaw"]
+
+# function 
+def allocateHouse():
+  label.configure(text=(random.choice(houses)))
+
+# configure the tkinter Graphical User Interface (GUI).
+root = tkinter.Tk()
+
+# Set the tkinter window title.
+root.title("Sorting Hat")
+
+# Set the tkinter window size.
+root.geometry("600x100")
+
+# Set the font style and size for tkinter.
+
+label = tkinter.Label(root, text="", font=("Helvetica", 32))
+
+label.pack()
+
+# Create a button with label 
+insultButton = tkinter.Button(text="See results", command=allocateHouse)
+insultButton.pack()
+
+root.mainloop()
